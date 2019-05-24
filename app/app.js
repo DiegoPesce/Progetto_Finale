6677f1f7a3c1520858ee29bd415df9dc0794d690
Phish18 <diego.pesce18@gmail.com>
2019-05-24 06:33:39 +0000

    Crypted and so on

--- app/app.js
+++ app/app.js
@@ -3,7 +3,28 @@
 const express = require('express');
 const app = express();
 const mongo = require('./modules/mongodb.js');
-const bodyparser = require('body-parser')
+const bodyparser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const Client = require('node-rest-client').Client;
+var client = new Client(); 
+const fs = require('fs');  
+const https = require('https');
+ 
+var server = https.createServer({
+  key: fs.readFileSync(__dirname + '/server.key'),
+  cert: fs.readFileSync(__dirname + '/server.cert')
+}, app);
+
+// Cors
+app.use(cors());
+
+// Session
+app.use(
+    session({
+      secret: 'password'
+    })
+);
 
 // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(bodyparser.urlencoded());
@@ -12,21 +33,84 @@ app.use(bodyparser.urlencoded());
 app.use(bodyparser.json());
 
 app.get('/', function (req, res) {
-    res.writeHead(302, {
-      'Location': '/register'
-    });
-    res.end();
+    res.send("It works!");
 });
 
 app.post('/register', function (req, res) {
-    var username = req.body.username;
-    var
-    mongo.insert()
-        .then(result => {
-            console.log(result)
+    var nome = req.body.nome;
+    var cognome = req.body.cognome;
+    var username = req.body.username; 
+    var password = req.body.password;
+    var email = req.body.email;
+    var data_nascita = req.body.data_nascita;
+     
+    mongo.insert({  
+            nome : nome,
+            cognome : cognome,
+            username : username,
+            password : password,
+            email : email,
+            data_nascita : data_nascita
         })
+        .then(result => {
+            if (result.result.n == 1) {
+                res.send(true);
+            }else{
+                res.send(false);
+            }
+        })
+        
 });
 
-app.listen(3000, function () {
+app.post('/login', function (req, res) {
+    
+    if (!req.session.logged){
+        
+        var username = req.body.username;
+        var password = req.body.password;
+        
+        mongo.login({
+                user : username,
+                password : password,
+            })
+        .then(result => {
+            if (result == 1) {
+                req.session.logged = true;
+                res.send(true);
+            }else{
+                req.session.logged = false;
+                res.send(false);
+            }
+        });
+        
+    }else{
+        res.send('Already logged');
+    }
+        
+});
+ 
+app.post('/unlock', function (req, res) {
+    if(req.session.logged){
+        var qr = req.body.qr;
+        client.post(url + '/unlock', qr, function (data, response) {
+            // parsed response body as js object
+            console.log(data);
+            // raw response
+            console.log(response);
+        });
+    }
+});
+
+app.get('/delete', function (req, res) {
+    mongo.cancellaMacelloMontini();
+    res.send("It works!");
+});
+
+
+server.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+
+///////////// functions
+
--- app/modules/mongodb.js
+++ app/modules/mongodb.js
@@ -15,5 +15,32 @@ module.exports = {
                 });
             });
         });
+    },
+    
+    login: function(obj){
+        return new Promise( (resolve, reject) => {
+            mongodb.connect(url, function(err, db) {
+                if (err) throw err;
+                var dbo = db.db("Progetto_Finale");
+                dbo.collection("Utenti")
+                  .countDocuments(obj, function(err, res) {
+                    if (err) reject(err);
+                    db.close();
+                    resolve(res);
+                });
+            });
+        });
+    },
+    
+    cancellaMacelloMontini: function(){
+        mongodb.connect(url, function(err, db) {
+                if (err) throw err;
+                var dbo = db.db("Progetto_Finale");
+                dbo.collection("Utenti")
+                  .remove({}, function(err, res) {
+                    if (err) throw err;
+                    db.close();
+                });
+            });
     }
 }
